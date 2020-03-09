0a5cd641d9623a166d61a9473810dd94963eddbb
Kranx-IN <vsashank.nanduri@gmail.com>
2020-03-09 10:57:40 +0530

    done by dev2

--- MobileProject/src/com/capgemini/ui/Main.java
+++ MobileProject/src/com/capgemini/ui/Main.java
@@ -1,10 +1,8 @@
 package com.capgemini.ui;
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("added by dev2");
 		System.out.println("now done by dev1");
 	}
-
 }
